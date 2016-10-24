cf4ec06227e4551cfeb2d0cb013d20404221a8fb
mayaghaei <aghaei.maya@gmail.com>
2016-10-24 17:11:34 +0200

    Update main.m

--- main.m
+++ main.m
@@ -13,7 +13,6 @@ addpath('faceDetector')
 
 inPath = 'input/';
 outPath = 'output/';
-addpath(inPath);
 inDir = dir(inPath);
 isDir = [inDir(:).isdir];
 inList = {inDir(isDir).name}';
@@ -31,36 +30,36 @@ colorThr = 12;%10;higher include more examples
 imgType = '.jpg';%change this if you have want to use another image type
 
 %% Generate Seeds
-% run('genSeed');
+run('genSeed');
 fprintf('Seed generation completed successfully! press any key to continue\n') ;
-% pause;
+pause;
 
 %% Tracking
 % Set the option to use deep-matching or sparse-matching!
-%run('Runtracker');%for Sparse matching
-% run('RuntrackerDeep')%for Deep matching
+% run('Runtracker');%for Sparse matching
+run('RuntrackerDeep')%for Deep matching
 fprintf('Tracklet generation completed successfully! press any key to continue\n') ;
-% pause;
+pause;
 
 %% Confidence
-% run ('calculateConfidence')
+run ('calculateConfidence')
 fprintf('Tracklet generation completed successfully! press any key to continue\n') ;
-% pause;
+pause;
 
 %% Bag-of-Tracklets
-% run('BOT');
+run('BOT');
 fprintf('BOT generation completed successfully! press any key to continue\n') ;
-% pause;
+pause;
 
 %% Exclude Unreliable BoTs
-% run('excludeUnBot');
+run('excludeUnBot');
 fprintf('Unreliable BOTs excluded successfully! press any key to continue\n') ;
-% pause;
+pause;
 
 %% Prototype Extraction
 run('prototype');
 fprintf('Prototypes extracted successfully! press any key to continue\n') ;
-% pause;
+pause;
 
 %% Occlusion treatment
 
